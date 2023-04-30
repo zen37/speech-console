0033f21836a665d7e5519c7a90df9f4948f25739
zen <zen@zen>
2023-04-29 17:57:39 -0700

    create audio file

--- speech2/Program.cs
+++ speech2/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel;
 using System.IO;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.CognitiveServices.Speech;
 using Microsoft.CognitiveServices.Speech.Audio;
@@ -14,68 +15,49 @@ class Program
     static string speechKey = Environment.GetEnvironmentVariable("SPEECH_KEY");
     static string speechRegion = Environment.GetEnvironmentVariable("SPEECH_REGION");
 
-
-
-    static void OutputSpeechSynthesisResult(SpeechSynthesisResult speechSynthesisResult, string text)
-    {
-        switch (speechSynthesisResult.Reason)
-        {
-            case ResultReason.SynthesizingAudioCompleted:
-                Console.WriteLine($"Speech synthesized for text: [{text}]");
-                break;
-            case ResultReason.Canceled:
-                var cancellation = SpeechSynthesisCancellationDetails.FromResult(speechSynthesisResult);
-                Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
-
-                if (cancellation.Reason == CancellationReason.Error)
-                {
-                    Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
-                    Console.WriteLine($"CANCELED: ErrorDetails=[{cancellation.ErrorDetails}]");
-                    Console.WriteLine($"CANCELED: Did you set the speech resource key and region values?");
-                }
-                break;
-            default:
-                break;
-        }
-    }
-
-    async static Task Main(string[] args)
+    static async Task Main(string[] args)
     {
         var speechConfig = SpeechConfig.FromSubscription(speechKey, speechRegion);
 
         // The language of the voice that speaks.
-        speechConfig.SpeechSynthesisVoiceName = "en-US-NancyNeural";
+        speechConfig.SpeechSynthesisVoiceName = "en-US-JennyNeural";
 
-        using (var speechSynthesizer = new SpeechSynthesizer(speechConfig))
+        // SSML to synthesize.
+      //  string ssml = "<speak version='1.0' xmlns='http://www.w3.org/2001/10/synthesis' xml:lang='en-US'><voice name='en-US-JennyNeural'><prosody rate='1.0'>Hello, world!</prosody></voice></speak>";
+        var assembly = Assembly.GetExecutingAssembly();
+        var resourceManager = new ResourceManager("speech2.Resource1", assembly);
+        var file = resourceManager.GetString("ssml");
+
+
+        using (var synthesizer = new SpeechSynthesizer(speechConfig))
         {
-            // Get text from the console and synthesize to the default speaker.
-            //   Console.WriteLine("Enter some text that you want to speak >");
-            //   string text = Console.ReadLine();
+            using (var result = await synthesizer.SpeakSsmlAsync(file))
+            {
+                if (result.Reason == ResultReason.SynthesizingAudioCompleted)
+                {
+                    using (var audioDataStream = AudioDataStream.FromResult(result))
+                    {
+                        // Save audio data to a file
+                        await audioDataStream.SaveToWaveFileAsync("output2.wav");
 
-            //var file = speech2.Resource1.text_txt;
+                        Console.WriteLine($"Audio file saved to {Path.GetFullPath("output.wav")}");
+                    }
+                }
+                else if (result.Reason == ResultReason.Canceled)
+                {
+                    var cancellation = SpeechSynthesisCancellationDetails.FromResult(result);
+                    Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
 
-            // var file = Properties.Resource1.text_txt;
-
-            var assembly = Assembly.GetExecutingAssembly();
-            var resourceManager = new ResourceManager("speech2.Resource1", assembly);
-            //var file = resourceManager.GetString("text");
-            //var speechSynthesisResult = await speechSynthesizer.SpeakTextAsync(file);
-            //   Console.WriteLine(resource);
-
-            var file = resourceManager.GetString("ssml");
-            var speechSynthesisResult = await speechSynthesizer.SpeakSsmlAsync(file);
-
-            // string fileName = @"..\text.txt";
-            // string text = File.ReadAllText(file);
-
-
-            // var speechSynthesisResult = await speechSynthesizer.SpeakSsmlAsync(file);
-
-            OutputSpeechSynthesisResult(speechSynthesisResult, file);
+                    if (cancellation.Reason == CancellationReason.Error)
+                    {
+                        Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                        Console.WriteLine($"CANCELED: ErrorDetails=[{cancellation.ErrorDetails}]");
+                    }
+                }
+            }
         }
 
         Console.WriteLine("Press any key to exit...");
         Console.ReadKey();
-
     }
 }
